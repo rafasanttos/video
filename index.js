d1f16be0fe5d825c039c0535034b25291ce00ab6
RAFAEL DOS SANTOS <79388000+rafasanttos@users.noreply.github.com>
2024-05-16 17:20:24 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+ScrollReveal().reveal('.texto-topao h1' ,{
+    scale: 2,
+    duration: 2000,
+});
+
+
+ScrollReveal().reveal('.texto-topao p' ,{
+    delay:1000,
+    distance:'90px',
+    duration:1000,
+
+
+});
+
+ScrollReveal().reveal('.inicial-img' ,{
+
+    opacity : 0.2,
+    duration:1000,
+
+
+});
+
+ScrollReveal().reveal('.efeito-apresentacao h2',{
+
+    distance:'50px',
+    duration:1500,
+
+
+});
+
+ScrollReveal().reveal('.efeito-apresentacao p',{
+    delay: 300,
+    distance:'50px',
+    duration:1500,
+
+
+});
+
+ScrollReveal().reveal('.efeito-sobre-primeira-caixa',{
+    duration: 2000,
+    distance: '300px',
+    origin:'left',
+    
+    
+
+
+});
+
+ScrollReveal().reveal('.efeito-sobre-segunda-caixa',{
+    delay: 700,
+    duration: 2000,
+    distance: '100px',
+    origin:'top',
+    
+    
+
+
+});
+
+ScrollReveal().reveal('.efeito-sobre-terceira-caixa',{
+    delay: 1000,
+    duration: 2000,
+    distance: '-300px',
+    origin:'left',
+    
+    
+
+
+});
+
+ScrollReveal().reveal('.botao-final',{
+    delay: 400,
+    duration: 2000,
+    scale:3
+    // distance: '90px',
+    // origin:'bottom',
+    
+    
+
+
+});
